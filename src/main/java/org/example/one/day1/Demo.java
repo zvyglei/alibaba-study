656f437320179c1d4faa21c8120517b64af34c22
zhao <leizhao866@163.com>
2021-02-15 23:00:20 +0800

    Initial commit

---
+++ src/main/java/org/example/one/day1/Demo.java
@@ -0,0 +1,23 @@
+package org.example.one.day1;
+
+/**
+ * @author zhao
+ * @time 2021/2/15 22:57
+ */
+public class Demo {
+    public static void main(String[] args) {
+        int i = '八';
+        // 20843
+        System.out.println(i);
+
+        char c = 20843;
+        // 八
+        System.out.println(c);
+
+
+        String a = "你";
+        String b = "好";
+        String ab = a + b;
+        System.out.println(a + "\n" + b);
+    }
+}
